c74b7c41638f228b27d44efcf612d693813e90b4
Rick <RTSmith801@gmail.com>
2026-02-23 11:47:54 -0700

    Testing new modern feel

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./styles/styles.scss";
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
+
+reveals.forEach((section) => {
+  observer.observe(section);
+});
